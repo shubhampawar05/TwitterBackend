60b3d11f590f3b122d70567d0adcc4b6df3910f3
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-27 11:39:15 +0530

    cookies

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,18 @@
 import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (userId, res) => {
-    console.log(process.env.JWT_SECRET);
+    // console.log(process.env.JWT_SECRET);
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	}); 
-
+	console.log("token form cookies function",token);
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
-		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		//httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "none",
+		secure:true,
+		path:'/' 
+		// CSRF attacks cross-site request forgery attacks
 		// secure: process.env.NODE_ENV !== "development",
 	});
 };

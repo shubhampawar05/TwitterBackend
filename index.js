f00d2516b049e34a453a4893ec2511ce80bbfc4b
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-28 20:52:52 +0530

    add frontend url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.Port || 10000;
 app.use(cookieParser());
 
 const corsOption = {
-  origin: ["http://localhost:5173","http://localhost:10000"],
+  origin: ["https://twitter-fullstack-pi.vercel.app/"],
   credentials: true,
 };
 

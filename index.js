c6eabbc36851274bc3aa5aace957ca27432d1368
Shubham Chopde <shubhamchopde090@gmail.com>
2024-06-29 11:12:18 +0530

    path

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.Port || 10000;
 app.use(cookieParser());
 
 const corsOption = {
-  origin: ["https://twitter-fullstack-pi.vercel.app/"],
+  origin: ["https://twitter-fullstack-pi.vercel.app"],
   credentials: true,
 };
 
